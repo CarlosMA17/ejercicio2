b01bb61963f1710fb8cf0f9347165cd92ff93b5e
unknown <carlosma0117@gmail.com>
2022-05-26 11:25:30 +0100

    Features: inicializacion de la clase, se le añadio los setter y getter y se le arreglo el constructor y se mejoraron los metodos de la clase

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        VeredaGT3000 prueba = new VeredaGT3000("HYL9430", 10, 5);
+
+        System.out.println(prueba.status());
+        prueba.move();
+        System.out.println(prueba.energyLevel());
+        System.out.println(prueba.status());
+
     }
 }
--- src/VeredaGT3000.java
+++ src/VeredaGT3000.java
@@ -5,31 +5,55 @@ public class VeredaGT3000 implements Vehicle{
     public int movementCount = 0;
     public int energyDelta;
 
-    public VeredaGT3000(String matricula, int maxEnergy, int energy, int movementCount, int energyDelta) {
+    public VeredaGT3000(String matricula, int energy, int energyDelta) {
         this.matricula = matricula;
-        this.maxEnergy = maxEnergy;
         this.energy = energy;
-        this.movementCount = movementCount;
+        this.energyDelta = energyDelta;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public float getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(float energy) {
+        this.energy = energy;
+    }
+
+    public int getEnergyDelta() {
+        return energyDelta;
+    }
+
+    public void setEnergyDelta(int energyDelta) {
         this.energyDelta = energyDelta;
     }
 
     @Override
     public float energyLevel() {
-        return 0;
+        return energy;
     }
 
     @Override
     public void recharge() {
-
+        energy = maxEnergy;
     }
 
     @Override
     public void move() {
-
+        energy = energy - energyDelta;
+        movementCount = movementCount+1;
     }
 
     @Override
     public String status() {
-        return null;
+        return "El vehículo <"+ matricula + "> tiene un nivel de carga de: <" + energy + "> y se ha movido <" + movementCount + "> veces";
     }
+
 }
